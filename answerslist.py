de3ab282aca29f1dd2d9d29f2f40c808bd604b18
Timofey <53184287+Timok951@users.noreply.github.com>
2025-03-13 23:09:01 +0300

    sgd

--- answerslist.py
+++ answerslist.py
@@ -10,14 +10,14 @@ answers = [
 ]
 
 work = [
-    "<@&1321746518314586143>, Time to work. Do not forget to: \n1)Open box\n2)Go on patrool\n 3)Two times go to event and kill mobs",
-    "<@&1321746518314586143>, Family, chop, chop, time for daily routine. Do not forget to:\n1)Open box\n2)Go on patrool\n 3)Two times go to event and kill ELIDS",
-    "<@&1321746518314586143>, I hope nobody forget to do their job, but I should notify you. Do not forget to: \n1)Open box\n 2)Go on patrool\n3)Two times go to event and kill ELIDS",
-    "<@&1321746518314586143>,New day new tasks you need to: \n1)Open box\n2)Go on patrool\n3)Two times go to event and kill ELIDS",
-    "<@&1321746518314586143>, All you need members, all you need are : \n1)Open box\n2)Go on patrool \n3)Two times go to event and kill ELIDS",
-    "<@&1321746518314586143>, I should remind you that you need to: \n1)Open box\n 2)Go on patrool \n3)Two times go to event and kill ELIDS",
-    "<@&1321746518314586143>, Do not forget to: \n1)Open box\n2)Go on patrool \n 3)Two times go to event and kill mobs",
-    "<@&1321746518314586143>, Please do the following steps: \n1)Open box\n2)Go on patrool\n3)Two times go to event and kill mobs"
+    "<@&1321746518314586143>, Time to work. Do not forget to: \n1)Open box\n2)Go on patrool",
+    "<@&1321746518314586143>, Family, chop, chop, time for daily routine. Do not forget to:\n1)Open box\n2)Go on patrool",
+    "<@&1321746518314586143>, I hope nobody forget to do their job, but I should notify you. Do not forget to: \n1)Open box\n 2)Go on patrool",
+    "<@&1321746518314586143>,New day new tasks you need to: \n1)Open box\n2)Go on patrool",
+    "<@&1321746518314586143>, All you need members, all you need are : \n1)Open box\n2)Go on patrool",
+    "<@&1321746518314586143>, I should remind you that you need to: \n1)Open box\n 2)Go on patrool",
+    "<@&1321746518314586143>, Do not forget to: \n1)Open box\n2)Go on patrool",
+    "<@&1321746518314586143>, Please do the following steps: \n1)Open box\n2)Go on patrool"
 ]
 
 workreminder = [
