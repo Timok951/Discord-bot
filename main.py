a165bc67230e3f550cd2eff440cd66d4a545187e
Timofey <53184287+Timok951@users.noreply.github.com>
2025-09-10 21:11:50 +0300

    fixed bug in replying

--- main.py
+++ main.py
@@ -259,8 +259,11 @@ async def on_message(message):
     # If 3 in row
     if previous["count"] == 3:
         logger.info("3 messages in a row, replying")
-        previous["count"] = 0  # The will be 0
+        previous["count"] = 0
+        previous["content"] = None
+        previous["channel"] = None
         await message.reply(content, mention_author=False)
+        previous["count"] = 0  # The will be 0
 
         
 
