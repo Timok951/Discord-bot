04d10f5e158d5ad1abe30f0ac1e2acbbaf0a9475
Timofey <53184287+Timok951@users.noreply.github.com>
2025-10-08 21:27:55 +0300

    Pasta and lowercase

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ async def on_message(message):
 
 
 
-    content = message.content
+    content = message.content.lower()
     channel_id = message.channel.id
     
     if content.startswith('Helian'):
@@ -323,22 +323,22 @@ async def on_message(message):
     
     #Pasts
     async with pastasend_lock:          
-        if ("лор волчицы" in content or "лор бураска" in content) or ("волчица" in content and "бурасик" in content) or ("волчица" in content and "бураск" in content) and pastasend == False:
+        if ( ("лор волчицы" in content or "лор бураска" in content) or ("волчица" in content and "бурасик" in content) or ("волчица" in content and "бураск" in content) ) and pastasend == False:
             await message.reply( pasts[3] ,mention_author=False)
             pastasend = True
             logger.info("Burask past was send")
 
-        if ("ниды" in content or "all you need" in content or "гансмок" in content) and pastasend == False:
+        if (("ниды" in content or "all you need" in content or "гансмок" in content)) and pastasend == False:
             await message.reply( pasts[0] ,mention_author=False)
             pastasend = True
             logger.info("Need past was send")
         
-        if ("деп" in content or "додеп" in content) and pastasend == False:
+        if (("деп" in content or "додеп" in content) )and pastasend == False:
             await message.reply( pasts[1] ,mention_author=False)
             pastasend = True
             logger.info("Dodep past was send")
     
-        if ("когда глобал" in content or "глобал" in content or "техи" in content or "технические работы" in content ) and pastasend == False:
+        if (("когда глобал" in content or "глобал" in content or "техи" in content or "технические работы" in content ) ) and pastasend == False:
             await message.reply( pasts[2] ,mention_author=False)
             pastasend = True
             logger.info("when global past was send")
